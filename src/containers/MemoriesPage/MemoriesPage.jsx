be962aaacae6458714ae9ec4a5a057f3153b80f0
Yuriy Dubinin <yuradubinin100@gmail.com>
2023-12-10 17:23:38 +0300

    Init

---
+++ src/containers/MemoriesPage/MemoriesPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './style/MemoriesPage.scss';
+
+const MemoriesPage = () => {
+    return <div className="memories-page">Memories</div>;
+};
+
+export default MemoriesPage;
---
+++ src/layouts/Layout.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './style/Layout.scss';
+
+import Header from './components/Header/Header';
+
+const Layout = ({children}) => {
+    return (
+        <div className="document">
+            <Header />
+            <main className="main-wrapper">
+                {/* <aside>{<MainSideBar />}</aside> */}
+                <div className="main-content">{children && children}</div>
+            </main>
+        </div>
+    );
+};
+
+export default Layout;
