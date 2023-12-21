2315c0b13e6ad195519071c95b47e99d69314b15
Yuriy Dubinin <yuradubinin100@gmail.com>
2023-12-21 21:15:30 +0300

    add

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import './style/Layout.scss';
 
+import LockedIcon from './assets/locked-user.svg?jsx';
+
 import Header from './components/Header/Header';
 
+import {getUser} from '../handlers/user.handler';
+
 const Layout = ({children}) => {
+    const [auth, setAuth] = useState(true);
+
+    useEffect(() => {
+        getUser();
+    }, []);
     return (
         <div className="document">
-            <Header />
-            <main className="main-wrapper">
-                {/* <aside>{<MainSideBar />}</aside> */}
-                <div className="main-content">{children && children}</div>
-            </main>
+            {!auth && (
+                <div className="locked__plug">
+                    <LockedIcon />
+                    <p>Необходимо авторизоваться</p>
+                </div>
+            )}
+            {auth && (
+                <>
+                    <Header />
+                    <main className="main-wrapper">
+                        {/* <aside>{<MainSideBar />}</aside> */}
+                        <div className="main-content">{children && children}</div>
+                    </main>
+                </>
+            )}
         </div>
     );
 };
